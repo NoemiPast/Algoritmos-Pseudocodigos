b345320017da36ccb208ab89664cb9e8aa1d31ae
NoemiPast <lic.noemipastrana@gmail.com>
2025-02-10 10:49:40 -0600

    Add files via upload
    De seudocódigo a código

---
+++ 1ertrabajo.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+	# Solicitar los 5 numeros para promediarlos
+	print("Ingresa 5 numeros para promediarlos, uno por uno")
+	uno = float(input("uno:"))
+	dos = float(input("dos:"))
+	tres = float(input("tres:"))
+	cuatro = float(input("cuatro:"))
+	cinco = float(input("cinco:"))
+	promedio = (uno+dos+tres+cuatro+cinco)/5
+	# Promediarlos
+	print("El promedio de las 5 calificaciones es:" ,promedio)
+	
+
