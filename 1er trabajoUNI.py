271656d8547ebcab71e1ab87c1845e46781cff51
NoemiPast <lic.noemipastrana@gmail.com>
2025-02-27 17:59:48 -0600

    Add files via upload

---
+++ 1er trabajoUNI.py
@@ -0,0 +1,44 @@
+print("Hello CodeSandbox!")
+print("Noemí")
+print("boxeo")
+print("not time")
+print("videojuegos")
+print("soltero")
+print("player")
+print("Vania es muy linda")
+print("Alan tiene una forma de ser muy curiosa")
+print("Ellos son primos")
+
+""" vamos a concatenar """#alt+shift+a
+nombre = "Noemí"
+apellido = "Pastrana"
+print("Hola mi nombre es:", nombre)
+print("Hola mi nombre es:", nombre, apellido)
+print("Hola mi nombre es:", nombre, "y mi apellido es:", apellido)
+
+""" concatenación con diferentes tipos de datos  """
+mi_edad=24
+mi_nombre="Noemí"
+mi_apellido_paterno="Pastrana"
+mi_apellido_materno="Gil"
+soy_maestro= False
+soy_estudiante= True
+
+print("mi edad es:", mi_edad, ", mi nombre es:", mi_nombre, ", mi apellido paterno es:",
+      mi_apellido_paterno,", mi apellido materno es", mi_apellido_materno,", ¿soy maestro?", 
+      soy_maestro,", ¿soy estudiante?",soy_estudiante)
+
+""" uso de type - Averigua que tipo de dato recibes """
+print(type(mi_edad))# <class 'int'>
+print(type(mi_nombre))# <class 'str'>
+print(type(soy_estudiante)) # <class 'bool'>
+
+""" Operaciones matematicas """
+numero_uno=15
+numero_dos=10
+
+print(numero_uno+numero_dos) #25
+print(numero_uno-numero_dos) #5
+print(numero_uno*numero_dos) #150
+print(numero_uno/numero_dos) #1.0
+print(numero_uno%numero_dos) #5
