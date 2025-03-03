53dac1b43d8672e6d566f638221e07d6782b5d6f
NoemiPast <lic.noemipastrana@gmail.com>
2025-03-03 11:56:48 -0600

    Add files via upload

---
+++ 3.1 Actividad de clase.py
@@ -0,0 +1,26 @@
+
+"""Lenguajes TIPADOS: requieren declarar las variables con su tipo de dato
+Y los lenguajes NO TIPADOS: no requieren declarar las variables con su tipo de dato, como Python"""
+
+#Se asigna un valora la variable numero
+"""numero = 10"""
+#Se imprime el valor de la variable numero, ej. 10
+"""print("El numero asignado a la variable numero es: ", numero)"""
+
+"""
+ACTIVIDAD DE CLASE:
+Definir los valores de variables e imprimir. Referente un hobby
+Usar:
+- Enteros
+- Booleanos
+_ Caracteres
+Tiempo 8 min
+"""
+
+#VENTA DE TACOS
+Tacos_De_Carne = 25
+Tacos_De_Guisado = 10
+Tacos_De_Canasta = 10
+
+print("Los tacos de carne son mas caros que los tacos de guisado: ", Tacos_De_Carne > Tacos_De_Guisado)
+print("Los tacos de guisado son mas baratos que los tacos de canasta: ", Tacos_De_Guisado < Tacos_De_Canasta)
---
+++ 3.2 Actividad If - Else.py
@@ -0,0 +1,18 @@
+#Se asigna un valora la variable numero
+numero = 10
+#Se imprime el valor de la variable numero, ej. 10
+"""print("El numero asignado a la variable numero es: ", numero)"""
+
+"""SENTENCIA IF - ELSE"""
+if numero <= 20:
+    print("El numero es menor o igual a 20")
+else:
+    print("El numero es mayor a 20")
+
+
+"""ACTIVIDAD: Crear 1 IF-ELSE al azar"""
+Calificacion = 9
+if Calificacion <= 9:
+    print("Aprobado")
+else: 
+    print("Reprobado")
